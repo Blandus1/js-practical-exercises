25ccd3c609731f43d18e7156ab6c24bad7dd0aac
Blandus1 <blandine0045@gmail.com>
2025-08-22 15:21:54 +0200

    prototype based

--- prototype.js
+++ prototype.js
@@ -9,21 +9,39 @@ NewPerson.prototype.greet=function(){
 return `hello,I am ${this.name},${this.age} years old thanks!`
 }
 
-// Child constructor
+//B. Child constructor
 function Student(name, age, grade) {
   NewPerson.call(this, name, age); // Inherit properties
   this.grade = grade;
 }
-// Inherit methods
+//C.Inherit methods
 Student.prototype = Object.create(NewPerson.prototype);//NewPerson object is created and assigned to Student so that instances of Student can access methods in NewPerson
 Student.prototype.constructor = Student;//reminds that student belongs to student not NewPerson
 
-// Add new method to Student
+//C.Add new method to Student
 Student.prototype.study = function() {
   return `${this.name} is studying in grade ${this.grade}.`;
 };
+
+//B.Instance of student
 const john = new Student('John',14,8)
 
 console.log(john.greet())
 console.log(john.study())
+
+//D. custom object with object.create()
+const StudentPrototype={
+greet : function (){
+console.log( `Hello I am ${this.name} from the custom object we are in the same ${this.grade} grade.`)
+},
+study: function (){
+  console.log(`${this.grade}th grade is the coolest`)
+}
+}
+const proto= Object.create(StudentPrototype)
+proto.name="Motton";
+proto.grade= 8;
+
+proto.greet()
+proto.study()
 
