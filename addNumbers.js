6a15527d08636fdf18d2937919de409fbc1e5214
Blandus1 <blandine0045@gmail.com>
2025-08-25 08:54:14 +0200

    adding error handling

--- addNumbers.js
+++ addNumbers.js
@@ -1,8 +1,13 @@
 
 const calculateExpression =(exp)=>{
-
-    const expression = new Function(` return ${exp}`)
+try{
+ const expression = new Function(` return ${exp}`)
     return expression ()
+}catch(error){
+       console.error("Error evaluating expression:", error);
+    return NaN; 
+}
+   
     }
 
 console.log(calculateExpression("( 2 + 3 ) * ( 4 - 1 )"))
